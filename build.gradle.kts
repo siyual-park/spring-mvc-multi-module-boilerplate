7785a3a746b9875c9d6eec35957a4a244d44668a
ara.bak <siyual.bak@gmail.com>
2021-09-06 17:15:34 +0900

    fix error

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,13 +34,13 @@ allprojects {
     }
 }
 
+tasks.withType<Jar> { duplicatesStrategy = DuplicatesStrategy.INHERIT }
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
         jvmTarget = "11"
     }
 }
-
 tasks.withType<Test> {
     useJUnitPlatform()
 }
