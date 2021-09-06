e26e59205798f7df6486da234edfa65af61af123
Ara Park <kdPark0723@gmail.com>
2021-09-06 17:30:49 +0900

    feat: add mvc (#1)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,11 +9,19 @@ fun includeModules(pathname: String) {
 }
 
 pluginManagement {
-    val klint_version: String by settings
     val kotlin_version: String by settings
+    val klint_version: String by settings
+    val spring_boot_version: String by settings
+    val spring_dependency_management_version: String by settings
 
     plugins {
+        id("org.springframework.boot") version spring_boot_version
+        id("io.spring.dependency-management") version spring_dependency_management_version
+
         kotlin("jvm") version kotlin_version
+        kotlin("plugin.spring") version kotlin_version
+        kotlin("plugin.jpa") version kotlin_version
+
         id("org.jlleitschuh.gradle.ktlint") version klint_version
     }
 }
